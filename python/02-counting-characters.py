54195f169cbbae6872d6e9ec0b6acc879c93df37
Rob Wells <rob@robjwells.com>
2019-07-17 22:26:31 +0100

    Python: Ex 2: tkinter attempt
    tkinter has a big problem, in that when the function is
    triggered, the text entry field has not registered the
    new content.

--- python/02-counting-characters.py
+++ python/02-counting-characters.py
@@ -1,4 +1,10 @@
-def main():
+import tkinter as tk
+
+
+EMPTY_MESSAGE = "The input is empty."
+
+
+def command_line_main():
     try:
         while True:
             string = input("What is the input string? ")
@@ -7,8 +13,39 @@ def main():
                 continue
             print(f"{string} has {len(string)} characters.")
     except (EOFError, KeyboardInterrupt):
-        pass    # User has quit
+        pass  # User has quit
 
 
-if __name__ == '__main__':
+def update_readout(input_field, output_field, event):
+    print(event, input_field.get())
+    input_string = input_field.get()
+    print([input_field.get() for _ in range(10)])
+    if not input_string:
+        output_field["text"] = EMPTY_MESSAGE
+    else:
+        output_field["text"] = f"{input_string} has {len(input_string)} characters"
+
+
+def main():
+    root = tk.Tk()
+    message = tk.Message(master=root, text="Enter a string:", width=400)
+    message.pack(padx=20, pady=10, fill="both")
+
+    text_field = tk.Entry(master=root, text="")
+    text_field.pack(padx=20, pady=10, fill="both")
+
+    readout = tk.Message(master=root, text=EMPTY_MESSAGE, width=400)
+    readout.pack(padx=20, pady=10, fill="both")
+
+    text_field.bind(
+        "<KeyPress>",
+        lambda ev: update_readout(
+            input_field=text_field, output_field=readout, event=ev
+        ),
+    )
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
     main()
